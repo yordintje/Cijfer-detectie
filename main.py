419705099c8babcabbcbf33d2f2a422243c851f3
yordin koning <1012241@leerling.tabor.nl>
2024-06-28 18:01:30 +0200

    starting code

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from torchvision import datasets
+from torchvision.transforms import ToTensor
 
+train_data = datasets.MNIST(
+    root = 'data',
+    train = True,
+    transform = ToTensor(),
+    download = True
+)
+
+test_data = datasets.MNIST(
+    root = 'data',
+    train = False,
+    transform = ToTensor(),
+    download = True
+)
