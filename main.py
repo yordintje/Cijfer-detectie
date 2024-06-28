46c9579ed1ff4e7a08531371a574dc2063951b86
Yordin Koning <89543459+Yordin-Koning@users.noreply.github.com>
2024-06-28 21:56:08 +0200

    Cijfer herkenning model

--- main.py
+++ main.py
@@ -20,13 +20,24 @@ import tensorflow as tf # Using for the Machine Learning part
 
 # model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy']) # Compile the model
 
-# model.fit(x_train, y_train, epochs=3) # Train the model
+# model.fit(x_train, y_train, epochs=5) # Train the model
 
 # model.save('geschreven.keras') # Save the model
 
 model = tf.keras.models.load_model('geschreven.keras') # Load the model
 
 image_number = 1
-while os.path.isfile(f"getallen/0/0_{image_number}.jpg"):
+while os.path.isfile(f"getallen/getal{image_number}.png"):
+    try:
+        img = cv2.imread(f"getallen/getal{image_number}.png")[:,:,0] # Load the image
+        img = np.invert(np.array([img])) # Invert the image
+        prediction = model.predict(img) # Predict the image
+        print(f"Het getal is waarschijnlijk een {np.argmax(prediction)}") # Print the prediction
+        plt.imshow(img[0], cmap=plt.cm.binary) # Show the image
+        plt.show()
+    except:
+        print("Error")
+    finally:
+        image_number += 1
 
 
